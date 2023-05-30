71c3e2f0c666e438576eed28ca7a4c8eddf48571
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2023-05-30 00:49:12 -0300

    correção json

--- scripts/api.js
+++ scripts/api.js
@@ -1,5 +1,6 @@
 async function fetchApiData() {
-  const url = '../data/configurations.json';
+  const url = 'https://raw.githubusercontent.com/luisantoniofreitas/cv/main/data/configurations.json';
+  // const url = '../data/configurations.json';
   const response = await fetch(url);
   const apiData = await response.json();
   return apiData;
